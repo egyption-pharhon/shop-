29479edc26ff0485b447982e5e5ea75202fa5c55
egyption-pharhon <egyptionpharhon@gmail.com>
2024-09-17 00:50:17 +0200

    Update items.js

--- items.js
+++ items.js
@@ -20,43 +20,10 @@ fetch('product.json')
 						<button type="" data-id=${product.id} onclick="openProduct(this.data-id)">Buy Now</button>
 					</div>`
 			})
-			function openProduct(id){
-				window.open("product.html", "_self")
-					let allProducts =[...data.children]
-				let element = allProducts[id]
-			const allIdForProducts = document.querySelector('.product')
-				allIdForProducts.innerHTML =`<div class="container">
-				<div class="images">
-					<img src="${element.img}" class="actaul">
-					<div class="color-product">
-						<img src="${element.productColors[0]}">
-						<img src="${element.productColors[1]}">
-						<img src="${element.productColors[2]}">
-						<img src="${element.productColors[3]}">
-					</div>
-				</div>
-				<div class="info">
-					<span>${element.featured}</span>
-					<h3>${element.name}</h3>
-					<span class="price">$${element.price}</span>
-					<select>
-						<option>Select Size</option>
-						<option>XL</option>
-						<option>XXL</option>
-						<option>Small</option>
-						<option>Large</option>
-					</select>
-					<input type="number" value="1">
-					<button>Add to Cart</button>
-					<h4>Product Details</h4>
-					<p>The Gildan Ultra Cotton T-shirt is made from a substantial 6.0 oz. per sq.
-					yd, fabric constructed from 100% cotton, this classic fit preshrunk jersey knit provides
-					unmatched comfort with each wear. Featuring a taped neck and shoulder, and a seamless
-					double- needle collar, and available in a range of colors,
-					it offers it all in the ultimate head-tuming package.</p>
-				</div>
-			</div>`
-		}
+			function openProduct(id) {
+    window.location.href = `product.html?id=${id}`;
+}
+
 
 
 	});
